b506b392135d73fe309e8608467bd43672aa5934
Andrey Sitnik <andrey@sitnik.ru>
2017-08-05 17:29:03 -0400

    Add distribution test to unit tests

--- test/generate.test.js
+++ test/generate.test.js
@@ -3,3 +3,24 @@ var generate = require('../generate')
 it('has options', function () {
   expect(generate('a', 5)).toEqual('aaaaa')
 })
+
+it('has flat distribution', function () {
+  var COUNT = 100 * 1000
+  var LENGTH = 5
+  var ALPHABET = 'abcdefghijklmnopqrstuvwxyz'
+
+  var chars = { }
+  for (var i = 0; i < COUNT; i++) {
+    var id = generate(ALPHABET, LENGTH)
+    for (var j = 0; j < id.length; j++) {
+      var char = id[j]
+      if (!chars[char]) chars[char] = 0
+      chars[char] += 1
+    }
+  }
+
+  for (var k in chars) {
+    var distribution = (chars[k] * ALPHABET.length) / (COUNT * LENGTH)
+    expect(distribution).toBeCloseTo(1, 1)
+  }
+})
--- test/index.test.js
+++ test/index.test.js
@@ -2,9 +2,8 @@ var nanoid = require('../')
 var url = require('../url')
 
 it('generates URL-friendly IDs', function () {
-  var id
   for (var i = 0; i < 10; i++) {
-    id = nanoid()
+    var id = nanoid()
     expect(id.length).toEqual(22)
     for (var j = 0; j < id.length; j++) {
       expect(url.indexOf(id[j])).not.toEqual(-1)
