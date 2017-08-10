bb3d6caf8e96de233e1d6ded3470aae30c25b71c
Andrey Sitnik <andrey@sitnik.ru>
2017-08-09 22:37:17 -0700

    Add separated flat distribution test for index

--- test/index.test.js
+++ test/index.test.js
@@ -20,3 +20,23 @@ it('has no collisions', function () {
     used[id] = true
   }
 })
+
+it('has flat distribution', function () {
+  var COUNT = 100 * 1000
+  var LENGTH = nanoid().length
+
+  var chars = { }
+  for (var i = 0; i < COUNT; i++) {
+    var id = nanoid()
+    for (var j = 0; j < id.length; j++) {
+      var char = id[j]
+      if (!chars[char]) chars[char] = 0
+      chars[char] += 1
+    }
+  }
+
+  for (var k in chars) {
+    var distribution = (chars[k] * url.length) / (COUNT * LENGTH)
+    expect(distribution).toBeCloseTo(1, 1)
+  }
+})
