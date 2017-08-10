3dbc388e1ada17e919e3c8bc831e763865421f19
Andrey Sitnik <andrey@sitnik.ru>
2017-08-09 22:36:52 -0700

    Fix format test for asking long byte sequence

--- test/format.test.js
+++ test/format.test.js
@@ -1,9 +1,13 @@
 var format = require('../format')
 
 it('generates random string', function () {
-  var bytes = [2, 255, 0, 1]
+  var sequence = [2, 255, 0, 1]
   function random (size) {
-    return bytes.slice(0, size)
+    var bytes = []
+    for (var i = 0; i < size; i += sequence.length) {
+      bytes = bytes.concat(sequence.slice(0, size - i))
+    }
+    return bytes
   }
   expect(format(random, 'abc', 4)).toEqual('cabc')
 })
