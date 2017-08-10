acdae9f03c5e18e2c6d890eccf0070dbb62af314
Andrey Sitnik <andrey@sitnik.ru>
2017-08-09 22:34:27 -0700

    Add benchmark to demo page

--- test/demo/index.js
+++ test/demo/index.js
@@ -12,58 +12,62 @@ for (var i = 0; i < 10; i++) {
 document.body.innerHTML = '<main>' + html + '</main>'
 
 var COUNT = 50 * 1000
-var STEP = 5000
 var ALPHABET = 'abcdefghijklmnopqrstuvwxyz'
 var LENGTH = ALPHABET.length
 
-function print (title, chars) {
-  var length = Object.keys(chars).length
-  var dots = ''
-
-  var average = Object.keys(chars).reduce(function (all, l) {
-    return all + chars[l]
-  }, 0) / length
-
-  Object.keys(chars).sort().forEach(function (l) {
-    var distribution = chars[l] / average
-    dots += '<div class="dot" style="' +
-      'background: hsl(' + (200 * distribution) + ', 100%, 50%); ' +
-      'width: ' + (100 / length) + '%; ' +
-    '">' + l + '</div>'
-  })
-
-  document.body.innerHTML += '<section>' +
-    '<h2>' + title + '</h2>' +
-    dots +
-  '</section>'
+function format (number) {
+  return String(Math.floor(number * 100))
+    .replace(/\d\d\d\d\d\d$/, ',$&')
+    .replace(/\d\d\d$/, ',$&')
 }
 
 function printDistribution (title, fn) {
-  var steps = COUNT
+  return calcDistribution(title, fn).then(function (data) {
+    var length = Object.keys(data.chars).length
+    var dots = ''
+
+    var average = Object.keys(data.chars).reduce(function (all, l) {
+      return all + data.chars[l]
+    }, 0) / length
+
+    Object.keys(data.chars).sort().forEach(function (l) {
+      var distribution = data.chars[l] / average
+      dots += '<div class="dot" style="' +
+        'background: hsl(' + (200 * distribution) + ', 100%, 50%); ' +
+        'width: ' + (100 / length) + '%; ' +
+      '">' + l + '</div>'
+    })
+
+    document.body.innerHTML += '<section>' +
+      '<span>' + format(COUNT * 1000 / data.time) + ' ops/sec</span>' +
+      '<h2>' + data.title + '</h2>' +
+      dots +
+    '</section>'
+  })
+}
+
+function calcDistribution (title, fn) {
   var chars = { }
 
   return new Promise(function (resolve) {
-    function step () {
-      if (steps === 0) {
-        print(title, chars)
-        resolve()
-        return
-      }
+    var ids = []
+    var j
 
-      setTimeout(function () {
-        for (var j = 0; j < STEP; j++) {
-          var id = fn()
-          for (var k = 0; k < id.length; k++) {
-            var char = id[k]
-            if (!chars[char]) chars[char] = 0
-            chars[char] += 1
-          }
-        }
-        steps -= STEP
-        step()
-      })
+    var start = Date.now()
+    for (j = 0; j < COUNT; j++) ids.push(fn())
+    var end = Date.now()
+
+    for (j = 0; j < COUNT; j++) {
+      var id = ids[j]
+      if (title === 'uuid/v4') id = id.replace(/-./g, '')
+      for (var k = 0; k < id.length; k++) {
+        var char = id[k]
+        if (!chars[char]) chars[char] = 0
+        chars[char] += 1
+      }
     }
-    step()
+
+    resolve({ title: title, chars: chars, time: end - start })
   })
 }
 
@@ -86,13 +90,8 @@ var tasks = [
     })
   },
   function () {
-    return printDistribution('shortid', function () {
-      return shortid()
-    })
-  },
-  function () {
     return printDistribution('uuid/v4', function () {
-      return uuid4().replace(/-./g, '')
+      return uuid4()
     })
   },
   function () {
@@ -101,6 +100,11 @@ var tasks = [
         return ALPHABET[num % ALPHABET.length]
       })
     })
+  },
+  function () {
+    return printDistribution('shortid', function () {
+      return shortid()
+    })
   }
 ]
 
@@ -108,7 +112,7 @@ function run () {
   if (tasks.length === 0) return
   var task = tasks.shift()
   task().then(function () {
-    run()
+    setTimeout(run, 10)
   })
 }
 
