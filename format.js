40d0e426aec3ecbefc1ce586a3a660ba9e14e561
mokhov <mokhov@yandex-team.ru>
2017-08-09 19:04:28 -0700

    Move bytes inside cycle, remove length lookup

--- format.js
+++ format.js
@@ -26,11 +26,10 @@
 module.exports = function (random, alphabet, size) {
   var step = Math.ceil(310 / alphabet.length * size)
 
-  var bytes
   var id = ''
   while (true) {
-    bytes = random(step)
-    for (var i = 0; i < bytes.length; i++) {
+    var bytes = random(step)
+    for (var i = 0; i < step; i++) {
       if (bytes[i] < alphabet.length) {
         id += alphabet[bytes[i]]
         if (id.length === size) return id
