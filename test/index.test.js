347413260f67148d32239fb112cf6fd35105d437
Andrey Sitnik <andrey@sitnik.ru>
2017-11-20 02:25:02 +0300

    Update dependencies

--- test/index.test.js
+++ test/index.test.js
@@ -4,7 +4,7 @@ var url = require('../url')
 it('generates URL-friendly IDs', function () {
   for (var i = 0; i < 10; i++) {
     var id = nanoid()
-    expect(id.length).toEqual(21)
+    expect(id).toHaveLength(21)
     for (var j = 0; j < id.length; j++) {
       expect(url.indexOf(id[j])).not.toEqual(-1)
     }
--- test/random.test.js
+++ test/random.test.js
@@ -2,7 +2,7 @@ var random = require('../random')
 
 it('generates random buffers', function () {
   var bytes = random(5)
-  expect(bytes.length).toEqual(5)
+  expect(bytes).toHaveLength(5)
   for (var i = 0; i < bytes.length; i++) {
     expect(typeof bytes[i]).toEqual('number')
   }
