889c1cc53ee4ad1575dc48d8a7d0af725693cb64
Andrey Sitnik <andrey@sitnik.ru>
2018-07-12 10:45:14 -0400

    Improve random() tests

--- test/random.test.js
+++ test/random.test.js
@@ -1,9 +1,14 @@
 var random = require('../random')
 
 it('generates random buffers', function () {
-  var bytes = random(5)
-  expect(bytes).toHaveLength(5)
+  var numbers = { }
+  var bytes = random(10000)
+  expect(bytes).toHaveLength(10000)
   for (var i = 0; i < bytes.length; i++) {
+    if (!numbers[bytes[i]]) numbers[bytes[i]] = 0
+    numbers[bytes[i]] += 1
     expect(typeof bytes[i]).toEqual('number')
+    expect(bytes[i]).toBeLessThanOrEqual(255)
+    expect(bytes[i]).toBeGreaterThanOrEqual(0)
   }
 })
