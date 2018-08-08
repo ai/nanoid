cb5a4873a60cb1937b42e3797cb4140a5e110560
Andrey Sitnik <andrey@sitnik.ru>
2018-08-08 09:24:18 -0400

    Add JSDoc to nanoid/non-secure

--- non-secure.js
+++ non-secure.js
@@ -1,5 +1,23 @@
 var url = '_~getRandomVcryp0123456789bfhijklqsuvwxzABCDEFGHIJKLMNOPQSTUWXYZ'
 
+/**
+ * Generate URL-friendly unique ID. This method use non-secure predictable
+ * random generator.
+ *
+ * By default, ID will have 21 symbols to have a collision probability similar
+ * to UUID v4.
+ *
+ * @param {number} [size=21] The number of symbols in ID.
+ *
+ * @return {string} Random string.
+ *
+ * @example
+ * const nanoid = require('nanoid/non-secure')
+ * model.id = nanoid() //=> "Uakgb_J5m9g~0JDMbcJqL"
+ *
+ * @name nonSecure
+ * @function
+ */
 module.exports = function (size) {
   size = size || 21
   var id = ''
