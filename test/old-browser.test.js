598f8869c31e8ffb8e3a974ce06895a0e0a27b3c
Andrey Sitnik <andrey@sitnik.ru>
2019-11-15 17:20:45 -0800

    Clean up tests

--- test/old-browser.test.js
+++ test/old-browser.test.js
@@ -1,5 +1,5 @@
 it('warns to use non-secure generator on old browsers', () => {
   expect(() => {
     require('../index.browser')
-  }).toThrowError(/use nanoid\/non-secure/)
+  }).toThrow(/use nanoid\/non-secure/)
 })
--- test/react-native.test.js
+++ test/react-native.test.js
@@ -5,5 +5,5 @@ global.navigator = {
 it('warns to use non-secure generator on old browsers', () => {
   expect(() => {
     require('../index.browser')
-  }).toThrowError(/React Native does not have/)
+  }).toThrow(/React Native does not have/)
 })
