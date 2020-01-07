3aa2ec365bd3a0e0cbcf5324ba291550828da1b3
Alexey Berezin <beraliv.spb@gmail.com>
2020-01-08 01:19:00 +0300

    docs: 🔄update case order for -~ meaning

--- async/format.browser.js
+++ async/format.browser.js
@@ -1,8 +1,8 @@
 module.exports = function (random, alphabet, size) {
   // TODO
   var mask = (2 << 31 - Math.clz32((alphabet.length - 1) | 1)) - 1
-  // -~i => i + 1 if n is integer number
   // -~f => Math.ceil(f) if n is float number
+  // -~i => i + 1 if n is integer number
   var step = -~(1.6 * mask * size / alphabet.length)
 
   function tick (id) {
--- format.browser.js
+++ format.browser.js
@@ -1,8 +1,8 @@
 module.exports = function (random, alphabet, size) {
   // TODO
   var mask = (2 << Math.log(alphabet.length - 1) / Math.LN2) - 1
-  // -~i => i + 1 if n is integer number
   // -~f => Math.ceil(f) if n is float number
+  // -~i => i + 1 if n is integer number
   var step = -~(1.6 * mask * size / alphabet.length)
   var id = ''
 
