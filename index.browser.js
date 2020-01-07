2735966076bfdb894190090007c9855694eaa2b5
Dominik Skalník <dominik.skalnik@heidelbergcement.com>
2020-01-07 15:27:21 +0100

    Fix: secure-random-number-generator not found in RN debug mode. https://github.com/ai/nanoid/issues/162

--- index.browser.js
+++ index.browser.js
@@ -1,11 +1,4 @@
 if (process.env.NODE_ENV !== 'production') {
-  if (typeof navigator !== 'undefined' && navigator.product === 'ReactNative') {
-    throw new Error(
-      'React Native does not have a built-in secure random generator. ' +
-      'If you don’t need unpredictable IDs, you can use `nanoid/non-secure`. ' +
-      'For secure ID install `expo-random` locally and use `nanoid/async`.'
-    )
-  }
   if (typeof self === 'undefined' || (!self.crypto && !self.msCrypto)) {
     throw new Error(
       'Your browser does not have secure random generator. ' +
