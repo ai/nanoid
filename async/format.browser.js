7a32491d9c553914e8346d3e78a332c07ed66e30
Dair Aidarkhanov <36963426+aidarkhanov@users.noreply.github.com>
2020-01-07 12:43:16 +0600

    Replace Math.ceil with the bit operation (#161)
    * Replace Math.ceil with the bit operation
    
    * Reverse the change with bit operation
    
    * Reverse the change with bit operation

--- async/format.browser.js
+++ async/format.browser.js
@@ -1,6 +1,6 @@
 module.exports = function (random, alphabet, size) {
   var mask = (2 << 31 - Math.clz32((alphabet.length - 1) | 1)) - 1
-  var step = Math.ceil(1.6 * mask * size / alphabet.length)
+  var step = -~(1.6 * mask * size / alphabet.length)
 
   function tick (id) {
     return random(step).then(function (bytes) {
