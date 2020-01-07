ea4e1624039411e2db8b93f49f261b0211c86030
Alexey Berezin <beraliv.spb@gmail.com>
2020-01-08 00:16:22 +0300

    docs: ➕add -~ meaning

--- async/format.browser.js
+++ async/format.browser.js
@@ -1,5 +1,7 @@
 module.exports = function (random, alphabet, size) {
   var mask = (2 << 31 - Math.clz32((alphabet.length - 1) | 1)) - 1
+  // -~i => i + 1 if n is integer number
+  // -~f => Math.ceil(f) if n is float number
   var step = -~(1.6 * mask * size / alphabet.length)
 
   function tick (id) {
--- format.browser.js
+++ format.browser.js
@@ -1,5 +1,7 @@
 module.exports = function (random, alphabet, size) {
   var mask = (2 << Math.log(alphabet.length - 1) / Math.LN2) - 1
+  // -~i => i + 1 if n is integer number
+  // -~f => Math.ceil(f) if n is float number
   var step = -~(1.6 * mask * size / alphabet.length)
   var id = ''
 
