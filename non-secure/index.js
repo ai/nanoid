ff0c6335b776bfbb9007e29b9415aba2a52f0a9c
Anton Khlynovskiy <subzey@gmail.com>
2020-01-21 00:05:19 +0300

    Non-secure: Generate alphabet using fromCharCode (#167)

--- non-secure/index.js
+++ non-secure/index.js
@@ -1,7 +1,25 @@
-// This alphabet uses a-z A-Z 0-9 _- symbols. Symbols order was changed
-// for better gzip compression. We use genetic algorithm to find the best order.
-// Check generator code at test/alphabet-genetic.
-var url = 'sOwnPropMN49CEiq-hXvHJdSymlFURTag61GQfuD8YIWz2Zk5xKB7LV30_Abject'
+// This alphabet uses a-z A-Z 0-9 _- symbols.
+// Despite the fact the source code is quite long, its entropy
+// is low and there are lots of duplicates - just what compressors
+// like GZIP and Brotli likes the best.
+var i
+var url = '_-' + String.fromCharCode(
+  // ASCII codes for 0...9
+  i = 48, i += 1, i += 1, i += 1, i += 1, i += 1, i += 1, i += 1,
+  i += 1, i += 1,
+
+  // ASCII codes for A...Z
+  i += 8, i += 1, i += 1, i += 1, i += 1, i += 1, i += 1, i += 1,
+  i += 1, i += 1, i += 1, i += 1, i += 1, i += 1, i += 1, i += 1,
+  i += 1, i += 1, i += 1, i += 1, i += 1, i += 1, i += 1, i += 1,
+  i += 1, i += 1,
+
+  // ASCII codes for a...z
+  i += 7, i += 1, i += 1, i += 1, i += 1, i += 1, i += 1, i += 1,
+  i += 1, i += 1, i += 1, i += 1, i += 1, i += 1, i += 1, i += 1,
+  i += 1, i += 1, i += 1, i += 1, i += 1, i += 1, i += 1, i += 1,
+  i += 1, i += 1
+)
 
 /**
  * Generate URL-friendly unique ID. This method use non-secure predictable
