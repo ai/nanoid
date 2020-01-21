38c1e11071c875f15b5e3848c331428904d788a2
Anton Khlynovskiy <subzey@gmail.com>
2020-01-21 23:17:43 +0300

    Use fromCharCode to generate the alphabet everywhere (#168)

--- url.js
+++ url.js
@@ -1,6 +1,9 @@
-// This alphabet uses a-z A-Z 0-9 _- symbols. Symbols order was changed
-// for better gzip compression. We use genetic algorithm to find the best order.
-// Check generator code at test/alphabet-genetic.
+// This alphabet uses a-z A-Z 0-9 _- symbols.
+// Despite the fact the source code is quite long, its entropy
+// is low and there are lots of duplicates - just what compressors
+// like GZIP and Brotli likes the best.
+
+var i
 
 /**
  * URL safe symbols.
@@ -12,5 +15,20 @@
  * const url = require('nanoid/url')
  * generate(url, 10) //=> "Uakgb_J5m9"
  */
-module.exports =
-  'ModuleSymbhasOwnPr-0123456789ABCDEFGHNRVfgctiUvz_KqYTJkLxpZXIjQW'
+module.exports = '_-' + String.fromCharCode(
+  // ASCII codes for 0...9
+  i = 48, i += 1, i += 1, i += 1, i += 1, i += 1, i += 1, i += 1,
+  i += 1, i += 1,
+
+  // ASCII codes for A...Z
+  i += 8, i += 1, i += 1, i += 1, i += 1, i += 1, i += 1, i += 1,
+  i += 1, i += 1, i += 1, i += 1, i += 1, i += 1, i += 1, i += 1,
+  i += 1, i += 1, i += 1, i += 1, i += 1, i += 1, i += 1, i += 1,
+  i += 1, i += 1,
+
+  // ASCII codes for a...z
+  i += 7, i += 1, i += 1, i += 1, i += 1, i += 1, i += 1, i += 1,
+  i += 1, i += 1, i += 1, i += 1, i += 1, i += 1, i += 1, i += 1,
+  i += 1, i += 1, i += 1, i += 1, i += 1, i += 1, i += 1, i += 1,
+  i += 1, i += 1
+)
