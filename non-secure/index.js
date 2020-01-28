bd49e7ec2f6c319fbc98ce513d240d0207eab9ea
gwer <webholt@gmail.com>
2020-01-28 20:46:23 +0300

    Update non-secure index.js

--- non-secure/index.js
+++ non-secure/index.js
@@ -2,24 +2,13 @@
 // Despite the fact the source code is quite long, its entropy
 // is low and there are lots of duplicates - just what compressors
 // like GZIP and Brotli likes the best.
-var i
-var url = '_-' + String.fromCharCode(
-  // ASCII codes for 0...9
-  i = 48, i += 1, i += 1, i += 1, i += 1, i += 1, i += 1, i += 1,
-  i += 1, i += 1,
+var i = 36
+var url = '-_'
 
-  // ASCII codes for A...Z
-  i += 8, i += 1, i += 1, i += 1, i += 1, i += 1, i += 1, i += 1,
-  i += 1, i += 1, i += 1, i += 1, i += 1, i += 1, i += 1, i += 1,
-  i += 1, i += 1, i += 1, i += 1, i += 1, i += 1, i += 1, i += 1,
-  i += 1, i += 1,
-
-  // ASCII codes for a...z
-  i += 7, i += 1, i += 1, i += 1, i += 1, i += 1, i += 1, i += 1,
-  i += 1, i += 1, i += 1, i += 1, i += 1, i += 1, i += 1, i += 1,
-  i += 1, i += 1, i += 1, i += 1, i += 1, i += 1, i += 1, i += 1,
-  i += 1, i += 1
-)
+while (i--) {
+  url += i.toString(36)
+  i > 9 && (url += i.toString(36).toUpperCase())
+}
 
 /**
  * Generate URL-friendly unique ID. This method use non-secure predictable
@@ -37,8 +26,8 @@ var url = '_-' + String.fromCharCode(
  * @function
  */
 module.exports = function (size) {
-  size = size || 21
   var id = ''
+  size = size || 21
   // Compact alternative for `for (var i = 0; i < size; i++)`
   while (size--) {
     // `| 0` is compact and faster alternative for `Math.floor()`
