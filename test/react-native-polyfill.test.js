4d835c0452a9668ee3b170d097e3a95e73913fcb
Andrey Sitnik <andrey@sitnik.ru>
2020-03-23 17:28:40 -0400

    Improve React Native tests

---
+++ test/react-native-polyfill.test.js
@@ -0,0 +1,19 @@
+global.navigator = {
+  product: 'ReactNative'
+}
+global.self = {
+  crypto: {
+    getRandomValues (array) {
+      for (let i = 0; i < array.length; i++) {
+        array[i] = Math.floor(Math.random() * 256)
+      }
+      return array
+    }
+  }
+}
+
+let { nanoid } = require('../index.browser')
+
+it('works with polyfill', () => {
+  expect(typeof nanoid()).toEqual('string')
+})
--- test/react-native.test.js
+++ test/react-native.test.js
@@ -3,7 +3,7 @@ global.navigator = {
 }
 global.self = { }
 
-it('warns to use non-secure generator on old browsers', () => {
+it('tells to use non-secure generator on old browsers', () => {
   expect(() => {
     require('../index.browser')
   }).toThrow(/React Native does not have/)
