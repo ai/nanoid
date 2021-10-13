1fd06c279aaacc45319c7c84f166763088e0355b
Anton Khlynovskiy <akhlynovskiy@tradingview.com>
2021-10-13 12:43:52 +0300

    feat: Re-optimize alphabet for Brotli (only chars unused by gzip backrefs)

--- url-alphabet/index.js
+++ url-alphabet/index.js
@@ -1,6 +1,7 @@
 // This alphabet uses `A-Za-z0-9_-` symbols.
-// The order of characters is optimized for better gzip compression
+// The order of characters is optimized for better gzip and brotli compression.
+// Same as in non-secure/index.js
 let urlAlphabet =
-  'use-0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ_abdfghjklmnopqvwxyzrict'
+  'useandom-26T198340PX75pxJACKVERYMINDBUSHWOLF_GQZbfghjklqvwyzrict'
 
 module.exports = { urlAlphabet }
