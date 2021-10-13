75237a629157ed1eacc702d49d27a0d03cf3ff50
Anton Khlynovskiy <subzey@gmail.com>
2021-10-14 01:52:27 +0300

    feat: Re-optimize alphabet for Brotli (only chars unused by gzip backrefs) (#310)
    * feat: Re-optimize alphabet for Brotli: Measure brotli sizes in size-limit
    
    * feat: Re-optimize alphabet for Brotli (only chars unused by gzip backrefs)
    
    Co-authored-by: Anton Khlynovskiy <akhlynovskiy@tradingview.com>

--- non-secure/index.js
+++ non-secure/index.js
@@ -1,7 +1,11 @@
 // This alphabet uses `A-Za-z0-9_-` symbols.
-// The order of characters is optimized for better gzip compression
+// The order of characters is optimized for better gzip and brotli compression.
+// References to the same file (works both for gzip and brotli):
+// `'use`, `andom`, and `rict'`
+// References to the brotli default dictionary:
+// `-26T`, `1983`, `40px`, `75px`, `bush`, `jack`, `mind`, `very`, and `wolf`
 let urlAlphabet =
-  'useandom-0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ_bfghjklpqvwxyzrict'
+  'useandom-26T198340PX75pxJACKVERYMINDBUSHWOLF_GQZbfghjklqvwyzrict'
 
 let customAlphabet = (alphabet, size) => {
   return () => {
