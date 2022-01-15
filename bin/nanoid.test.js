d50c47251d4c7f65df5e32a85efca2b90baf9a55
Vitaly Baev <ping@baev.dev>
2022-01-15 11:33:22 +0300

    Allows passing size or custom alphabet via cli as args

--- bin/nanoid.test.js
+++ bin/nanoid.test.js
@@ -1,15 +1,43 @@
-let { test } = require('uvu')
-let { is, match } = require('uvu/assert')
+let { suite } = require('uvu')
+let { is, match, equal } = require('uvu/assert')
 let { promisify } = require('util')
 let { join } = require('path')
 let child = require('child_process')
 
+let { parseArgs } = require('./parseArgs')
+
 let exec = promisify(child.exec)
 
-test('prints unique ID', async () => {
+const nanoIdSuit = suite('nanoid')
+
+nanoIdSuit('prints unique ID', async () => {
   let { stdout, stderr } = await exec('node ' + join(__dirname, 'nanoid.cjs'))
   is(stderr, '')
   match(stdout, /^[\w-]{21}\n$/)
 })
 
-test.run()
+nanoIdSuit('uses size', async () => {
+  let { stdout, stderr } = await exec('node ' + join(__dirname, 'nanoid.cjs') + ' --size=10')
+  is(stderr, '')
+  match(stdout, /^[\w-]{10}\n$/)
+})
+
+nanoIdSuit('uses alphabet', async () => {
+  let { stdout, stderr } = await exec('node ' + join(__dirname, 'nanoid.cjs') + ' --alphabet=abc --size=15')
+  is(stderr, '')
+  match(stdout, /^[abc]{15}\n$/)
+})
+
+nanoIdSuit.run()
+
+const parseArgsSuite = suite('parseArgs')
+
+parseArgsSuite('parses args', () => {
+  equal(parseArgs(['node', 'nanoid.cjs', '--help']), { help: true })
+  equal(parseArgs(['node', 'nanoid.cjs', '--help', '--size=30']), { help: true, size: '30' })
+  equal(parseArgs(['node', 'nanoid.cjs', '--help', '-s', '30']), { help: true, s: '30' })
+  equal(parseArgs(['node', 'nanoid.cjs', '--help', '-size', '30']), { help: true, size: '30' })
+  equal(parseArgs(['node', 'nanoid.cjs', '--help', '-size', '30', '-alphabet', 'abc']), { help: true, size: '30', alphabet: 'abc' })
+})
+
+parseArgsSuite.run()
