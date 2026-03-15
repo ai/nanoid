e0a30808016ad4f7d2af30a2dc53fada57cfe27f
Andrey Sitnik <andrey@sitnik.ru>
2026-03-15 18:05:55 +0000

    Clean up code

--- test/check-versions.js
+++ test/check-versions.js
@@ -4,12 +4,10 @@ import { readFileSync } from 'fs'
 import { join } from 'path'
 import { styleText } from 'node:util'
 
-let pkg = JSON.parse(
-  readFileSync(join(import.meta.dirname, '../package.json'), 'utf8')
-)
-let jsr = JSON.parse(
-  readFileSync(join(import.meta.dirname, '../jsr.json'), 'utf8')
-)
+const ROOT = join(import.meta.dirname, '..')
+
+let pkg = JSON.parse(readFileSync(join(ROOT, 'package.json'), 'utf8'))
+let jsr = JSON.parse(readFileSync(join(ROOT, 'jsr.json'), 'utf8'))
 
 if (pkg.version !== jsr.version) {
   process.stderr.write(
