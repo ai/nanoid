747ec4712cb48a7821bf46efee4bcda3a367c5a3
Mahmoud Hamdi <hmdy7486@gmail.com>
2026-03-15 16:21:29 +0200

    fix: update prebuild script and regenerate nanoid.js
    The update-prebuild.js script used replaceAll('urlAlphabet', 'a')
    to inline the alphabet, but index.browser.js now uses
    scopedUrlAlphabet (imported as alias). Since replaceAll is
    case-sensitive, 'urlAlphabet' did not match the uppercase 'Url'
    in 'scopedUrlAlphabet', producing broken output referencing an
    undefined variable.
    
    This also regenerates nanoid.js which was stale and missing the
    |= 0 pool pollution fix from v5.0.9.

--- nanoid.js
+++ nanoid.js
@@ -1,1 +1,1 @@
-let a="useandom-26T198340PX75pxJACKVERYMINDBUSHWOLF_GQZbfghjklqvwyzrict";export let nanoid=(e=21)=>{let t="",r=crypto.getRandomValues(new Uint8Array(e));for(let n=0;n<e;n++)t+=a[63&r[n]];return t};
+let a="useandom-26T198340PX75pxJACKVERYMINDBUSHWOLF_GQZbfghjklqvwyzrict";export let nanoid=(e=21)=>{let t="",r=crypto.getRandomValues(new Uint8Array(e|=0));for(;e--;)t+=a[63&r[e]];return t};
